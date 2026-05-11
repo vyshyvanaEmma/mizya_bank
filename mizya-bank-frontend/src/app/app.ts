2ceb66436ca03c2eaacf13cf9f7982c2654dfa1c
vyshyvanaEmma <vyshyvana.emma@itismeucci.com>
2026-05-11 09:11:52 +0200

    change nav and cut function that weren't used

--- mizya-bank-frontend/src/app/app.ts
+++ mizya-bank-frontend/src/app/app.ts
@@ -11,31 +11,15 @@ import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 export class App {
   isMenuOpen = false;
 
-  constructor() {
-    afterNextRender(() => {
-      setInterval(() => {
-        const players = document.querySelectorAll('.gd-player');
-        players.forEach(player => {
-          const rect = player.getBoundingClientRect();
-          if (rect.left > -20 && rect.left < window.innerWidth) {
-            const x = rect.left + (rect.width / 2);
-            const y = rect.top + rect.height;
-            this.createCoblestone(x, y);
-          }
-        });
-      }, 40);
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
 
-    });
+    // background scrolling when the menu is open
+    if (this.isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 
-  toggleMenu() { this.isMenuOpen = !this.isMenuOpen; }
-
-  createCoblestone(x: number, y: number) {
-    const p = document.createElement('div');
-    p.className = 'particle-neon'; 
-    p.style.left = x + 'px';
-    p.style.top = y + 'px';
-    document.body.appendChild(p);
-    setTimeout(() => p.remove(), 500);
-  }
 }
