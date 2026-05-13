f93ae8665136c37a8a9c5bb5bd0bd38a5322a3d0
Pmizya <vemma37257@gmail.com>
2026-05-13 21:39:34 +0200

    change db credentials

--- mini_banking_api/php/methods/MovimentiMethods.php
+++ mini_banking_api/php/methods/MovimentiMethods.php
@@ -4,13 +4,20 @@ class MovimentiMethods{
     
     private $db;
 
-    public function __construct(){
+    /*public function __construct(){
 
         $this->db = @new MySQLi(getenv('DB_HOST'),
                                 getenv('DB_USER'),
                                 getenv('DB_PASSWORD'),
                                 getenv('DB_NAME'));
+    }*/
+    public function __construct(){
+        $this->db = @new MySQLi('localhost',
+                                'root',
+                                '',
+                                'banca');
     }
+
 
      public function getConnection() {
         return $this->db;
